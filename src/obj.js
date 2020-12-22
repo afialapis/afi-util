85f152c13c004e0df526845ed1424d685b6ba777
afialapis <devel@afialapis.com>
2020-12-22 23:23:07 +0100

    adding objClone

--- src/obj.js
+++ src/obj.js
@@ -17,4 +17,40 @@ function objFilter(obj, func) {
   return out
 }
 
-export {isEmptyObject, objFilter}
+
+function objClone (obj) {
+  let copy
+
+  // Handle the 3 simple types, and null or undefined
+  if (null == obj || 'object' != typeof obj) return obj
+
+  // Handle Date
+  if (obj instanceof Date) {
+    copy = new Date()
+    copy.setTime(obj.getTime())
+    return copy
+  }
+
+  // Handle Array
+  if (obj instanceof Array) {
+    copy = []
+    for (let i = 0, len = obj.length; i < len; i++) {
+      copy[i] = objClone(obj[i])
+    }
+    return copy
+  }
+
+  // Handle Object
+  if (obj instanceof Object) {
+    copy = {}
+    for (let attr in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, attr)) copy[attr] = objClone(obj[attr])
+    }
+    return copy
+  }
+
+  throw new Error('Unable to copy obj! Its type isn\'t supported')
+}
+
+
+export {isEmptyObject, objFilter, objClone}
